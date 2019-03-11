86f3ffa52f9cca959d43a20d816629ad794e21ae
Nicolas Firavitoba <nicofira.dev@gmail.com>
2019-03-11 10:15:09 -0500

    Add functionality to chatroom, auto scroll bottom and clear input value

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,26 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = () => {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+// message_submit = () => {
+//   $('#message_body').keydown((e) => {
+//     if (e.which === 13) {
+//       $('button').click();
+//       e.target.value = ''
+//     }
+//   })
+// }
+
 $(document).on('turbolinks:load',() => {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  // message_submit();
+  scroll_bottom();
 })
